9a106dcd84140d8665f1f9d64ea58a75a67f4525
hweijing <jh13290900214@163.com>
2022-03-18 16:14:24 +0800

    2022.3.18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { format } from 'path';
 import { FormatService } from './services/format.service';
 import { FormatedSchool, School } from './types/interfaces';
 
